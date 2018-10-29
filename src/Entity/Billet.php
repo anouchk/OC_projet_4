ce0bd3ff488c73c91069a5c2e2691c2298624217
anouchk <analutzky@gmail.com>
2018-10-29 15:15:30 +0100

    création de l'entité Billet

---
+++ src/Entity/Billet.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BilletRepository")
+ */
+class Billet
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Pays;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $DateNaissance;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $TarifReduit;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $Tarif;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idCommande;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): self
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->Type;
+    }
+
+    public function setType(string $Type): self
+    {
+        $this->Type = $Type;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->Pays;
+    }
+
+    public function setPays(string $Pays): self
+    {
+        $this->Pays = $Pays;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->DateNaissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $DateNaissance): self
+    {
+        $this->DateNaissance = $DateNaissance;
+
+        return $this;
+    }
+
+    public function getTarifReduit(): ?bool
+    {
+        return $this->TarifReduit;
+    }
+
+    public function setTarifReduit(bool $TarifReduit): self
+    {
+        $this->TarifReduit = $TarifReduit;
+
+        return $this;
+    }
+
+    public function getTarif(): ?int
+    {
+        return $this->Tarif;
+    }
+
+    public function setTarif(?int $Tarif): self
+    {
+        $this->Tarif = $Tarif;
+
+        return $this;
+    }
+
+    public function getIdCommande(): ?int
+    {
+        return $this->idCommande;
+    }
+
+    public function setIdCommande(int $idCommande): self
+    {
+        $this->idCommande = $idCommande;
+
+        return $this;
+    }
+}
