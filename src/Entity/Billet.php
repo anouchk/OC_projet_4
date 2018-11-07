e6cced67fa95f94f0afcd486f66be052329b7afc
anouchk <analutzky@gmail.com>
2018-11-08 00:32:35 +0100

    ajout d'une contrainte de validation

--- src/Entity/Billet.php
+++ src/Entity/Billet.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BilletRepository")
@@ -18,11 +19,13 @@ class Billet
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $nom;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $prenom;
 
