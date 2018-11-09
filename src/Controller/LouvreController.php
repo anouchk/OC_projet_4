0daef744d1b498e2d671646a0b022eea4106b81f
anouchk <analutzky@gmail.com>
2018-11-09 16:48:03 +0100

    fonction pour créer aléatoirement un numéro de référence

--- src/Controller/LouvreController.php
+++ src/Controller/LouvreController.php
@@ -67,6 +67,17 @@ class LouvreController extends AbstractController
         //     return $this->redirectToRoute('recap', ['id' => $commande->getId()])
         // }
 
+        public function random_reference($length=6)
+        {
+            $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $string = '';
+            for($i=0; $i<$length; $i++){
+                $string .= $chars[rand(0, strlen($chars)-1)];
+            }
+            return $string;
+        }
+
+
         
         return $this->render('louvre/billetterie.html.twig', [
             'controller_name' => 'LouvreController',
