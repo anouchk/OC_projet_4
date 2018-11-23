38fa8c81998f4f21b41f44e185c418fe3aa36b0e
anouchk <analutzky@gmail.com>
2018-11-23 16:37:37 +0100

    la contrainte sur le billet demi-journée s'applique à toute la classe commande

--- src/Validator/Constraints/DemiJournee.php
+++ src/Validator/Constraints/DemiJournee.php
@@ -7,4 +7,9 @@ use Symfony\Component\Validator\Constraint;
 class DemiJournee extends Constraint
 {
     public $message = "Vous ne pouvez pas réserver un billet à la journée pour aujourd'hui car il est 14h passées : prenez un billet demi-journée";
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
 } 
--- src/Validator/Constraints/DemiJourneeValidator.php
+++ src/Validator/Constraints/DemiJourneeValidator.php
@@ -5,17 +5,22 @@ namespace App\Validator\Constraints;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
-use App\Entity\Billet;
+use App\Entity\Commande;
 
 class DemiJourneeValidator extends ConstraintValidator
 {
-    public function validate($value, Constraint $constraint)
+    public function validate($commande, Constraint $constraint)
     {
         $now = new \DateTime();
-        if ($billet->getCommande()->getDateVisite()->format('Y-m-d') == $now->format('Y-m-d') && $now->format('H') < 14 && $value == 1) {
-            $this->context->buildViolation($constraint->message)
-                          ->setParameter('** value **', 'billet demi-journée')
+        foreach ($commande->getBillets() as $billet) {
+        	if (
+        		$commande->getDateVisite()->format('Y-m-d') == $now->format('Y-m-d') &&
+        		$now->format('H') > 14
+        		$billet->getTypeBillet() == 1
+        	) {
+        		$this->context->buildViolation($constraint->message)
                           ->addViolation();
-        }      
+        	}
+        }    
     }
 }
