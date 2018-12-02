1fba0fb54c98727af97430d44dea3378f3c4fea0
anouchk <analutzky@gmail.com>
2018-12-02 18:07:10 +0100

    on injecte Card et Charge

--- src/Service/Paiement.php
+++ src/Service/Paiement.php
@@ -20,5 +20,27 @@ class Paiement
 	public function paiement (Commande $commande, Request $request)
 	{
 		$token = $this->request->request->get('stripeToken');
+		$prix = $commande->getPrix();
+
+		Stripe::setApiKey("sk_test_aXFr9emkHBkD35pC6kAvXLi7");
+
+		try {
+            Charge::create[(
+                "amount" => $prixCommande * 100, // Amount in cents
+                "currency" => "eur",
+                "source" => $token,
+                "description" => "Paiement Stripe - Réservations Louvre"
+            ]);
+            $commande->setPaid(true);
+            $commande->setClient($client);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($commande);
+            $em->flush();
+            return $this->redirectToRoute("mail",  array('id' => $commande->getId()));
+        	} 
+        catch(Card $e) {
+            $this->addFlash("error","Le paiement n'est pas passé :(");
+            return $this->redirectToRoute("recap, array('id' => $commande->getId())");
+            // The card has been declined
 	}
 }
