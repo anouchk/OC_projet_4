def96d0409ef99ff0d771a75a176873109ec3a82
anouchk <analutzky@gmail.com>
2018-12-05 16:24:40 +0100

    mise en forme

--- tests/Service/BilletManagerTest.php
+++ tests/Service/BilletManagerTest.php
@@ -4,16 +4,19 @@ use App\Entity\Billet;
 use App\Entity\Commande;
 use App\Service\BilletManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
 class BilletManagerTest extends KernelTestCase
 {
     /**
      * @var Billet
      */
     private $billet;
+
     /**
      * @var Commande
      */
     private $commande;
+
     public function setUp()
     {
         $this->billet = $this->createMock(Billet::class);
@@ -21,6 +24,7 @@ class BilletManagerTest extends KernelTestCase
         $this->commande = $this->createMock(Commande::class);
         $this->commande->method('getDateVisite')->willReturn(new \DateTime('2019-12-01'));
     }
+    
     public function testGetAge()
     {
         $billetManager = new BilletManager();
