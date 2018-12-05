04f499b4e934fc02f9b7fca6f85a1fcbab93ddf9
anouchk <analutzky@gmail.com>
2018-12-05 15:59:10 +0100

    on déporte l'envoi de mail dans un service

--- src/Controller/LouvreController.php
+++ src/Controller/LouvreController.php
@@ -111,34 +111,10 @@ class LouvreController extends AbstractController
     /**
      * @Route("/confirmation/{id}", name="mail")
      */
-    public function mail(Request $request, \Swift_Mailer $mailer)
+    public function mail(Request $request, MailService $mail_service, CommandeManager $commande_manager)
     {
         $commande = $commande_manager->getCommande($request->attributes->get('id'));
-
-        $message = (new \Swift_Message('Confirmation de réservation - Louvre'))
-            ->setFrom('analutzky@gmail.com')
-            ->setTo('analutzky@gmail.com')
-            ->setBody(
-                $this->renderView(
-                    // templates/mail.html.twig
-                    'louvre/mail.html.twig',
-                    array('commande' => $commande)
-                ),
-                'text/html'
-            )
-            /*
-             * If you also want to include a plaintext version of the message
-            ->addPart(
-                $this->renderView(
-                    'mail.txt.twig',
-                    array('name' => $name)
-                ),
-                'text/plain'
-            )
-            */
-        ;
-
-        $mailer->send($message);
+        $mail_service->envoiConfirmationCommande($commande);
 
         return $this->render('louvre/bye.html.twig', [
             'controller_name' => 'LouvreController',
--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -2,10 +2,47 @@
 
 namespace App\Service;
 
-use App\Entity\Billet;
 use App\Entity\Commande;
+use Twig\Environment;
 
 class MailService
 {
-	
+	/**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * @var \Swift_Mailer
+     */
+    private $mailer;
+    
+    /**
+     * MailService constructor.
+     *
+     * @param Environment $twig
+     * @param \Swift_Mailer $mailer
+     */
+    public function __construct(Environment $twig, \Swift_Mailer $mailer)
+    {
+        $this->twig = $twig;
+        $this->mailer = $mailer;
+    }
+
+	public function envoiConfirmationCommande($commande)
+	{
+		$message = (new \Swift_Message('Confirmation de réservation - Louvre : '. $commande->getReference()))
+            ->setFrom($commande->getClient()->getMail())
+            ->setTo('analutzky@gmail.com')
+            ->setBody(
+                $this->twig->render(
+                // templates/mail.html.twig
+                    'louvre/mail.html.twig',
+                    array('commande' => $commande)
+                ),
+                'text/html'
+            )
+        ;
+        $this->mailer->send($message);
+	}
 }
