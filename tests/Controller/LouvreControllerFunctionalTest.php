6b60f15cccca021cbbb62f04d4aa64bed62be43e
anouchk <analutzky@gmail.com>
2018-12-11 16:00:22 +0100

    test fonctionnel

--- tests/Controller/LouvreControllerFunctionalTest.php
+++ tests/Controller/LouvreControllerFunctionalTest.php
@@ -2,18 +2,31 @@
 namespace App\Tests\Controller;
 
 use App\Controller\LouvreController;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase ;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class LouvreControllerFunctionalTest extends WebTestCase
 {
-	public function testIndexStatusCode()
+	public function testHomeStatusCode()
 	{
 		// on crée un navigateur
 		$client = self::createClient();
 		$client->request('GET', '/');
 
 		self::assertSame(
-			200,
+			Response::HTTP_OK,
+			$client->getResponse()->getStatusCode()
+		);
+	}
+
+	public function testInfosPratiquesStatusCode()
+	{
+		// on crée un navigateur
+		$client = self::createClient();
+		$client->request('GET', '/infospratiques');
+
+		self::assertSame(
+			Response::HTTP_OK,
 			$client->getResponse()->getStatusCode()
 		);
 	}
